0f088ccf0aefec4429747493499fb6b2a5b631e4
Shivanshu80 <shivamsoni11001@gmail.com>
2026-08-15 23:25:08 +0530

    third commit

--- src/authModule/auth.controller.ts
+++ src/authModule/auth.controller.ts
@@ -1,33 +1,34 @@
 import { Controller, Get, Post, Body, Delete, Put } from '@nestjs/common';
 import { AuthService } from './auth.service'
-import type { User } from './auth.service'
+import { UserDto } from './user.dto';
+import { User } from './user.schema';
 
 @Controller('auth')
 export class AuthController {
     constructor(private readonly authService: AuthService) { }
     @Get('users')
-    allUsers(): User[] {
+    allUsers(): Promise<User[]> {
         return this.authService.getUsers();
     }
 
     @Post('adduser')
-    addUser(@Body() user: User): User {
-        return this.authService.addUser(user);
+    addUser(@Body() userDto: UserDto): Promise<User> {
+        return this.authService.addUser(userDto);
     }
 
-    @Delete('removeuser')
-    removeUser(@Body() userEmail: User): User {
-        return this.authService.removeUser(userEmail);
-    }
+    // @Delete('removeuser')
+    // removeUser(@Body() userEmail: User): User {
+    //     return this.authService.removeUser(userEmail);
+    // }
 
-    @Get('singleuser')
-    singleUser(@Body() user: User): User {
-        return this.authService.singleUser(user);
-    }
+    // @Get('singleuser')
+    // singleUser(@Body() user: User): User {
+    //     return this.authService.singleUser(user);
+    // }
 
-    @Put('updateuser')
-    updateUser(@Body() existsUser:User):User{
-        return this.authService.updateUser(existsUser);
-    }
+    // @Put('updateuser')
+    // updateUser(@Body() existsUser:User):User{
+    //     return this.authService.updateUser(existsUser);
+    // }
 
 }
